e3242adaa17d3f53185ce11b29f9c1b11d7e1350
Andrew Hawley <ahawley785@gmail.com>
2020-05-05 18:31:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,7 +1,31 @@
 # Write your code here!
 def game_hash
   {
-  :home => {},
+  :home => {
+    :team_name => '',
+    :colors => '',
+    :players => [
+      {:player_name => '',
+       :number => '',
+       :shoe => '',
+       :points =>,
+       :rebounds =>,
+       :assists =>,
+       :steals =>,
+       :blocks =>,
+       :slam_dunks =>},
+       
+      {:player_name => '',
+       :number => '',
+       :shoe => '',
+       :points =>,
+       :rebounds =>,
+       :assists =>,
+       :steals =>,
+       :blocks =>,
+       :slam_dunks =>}
+    ]
+  },
   :away => {}
   }
 end
