a7a2b79ba207172feca26e63b8742423ae3bbcc8
Andrew Hawley <ahawley785@gmail.com>
2020-05-05 18:41:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,8 +2,33 @@
 def game_hash
   {
   :home => {
+    :team_name => 'Brooklyn Nets',
+    :colors => ['Black','White'],
+    :players => [
+      {:player_name => 'Alan Anderson',
+       :number => 0,
+       :shoe => 16,
+       :points => 22,
+       :rebounds => 12,
+       :assists => 12,
+       :steals => 3,
+       :blocks => 1,
+       :slam_dunks => 1},
+
+      {:player_name => '',
+       :number => '',
+       :shoe => '',
+       :points => '',
+       :rebounds => '',
+       :assists =>'',
+       :steals =>'',
+       :blocks =>'',
+       :slam_dunks =>''}
+    ]
+  },
+  :away => {
     :team_name => '',
-    :colors => '',
+    :colors => [],
     :players => [
       {:player_name => '',
        :number => '',
@@ -25,7 +50,6 @@ def game_hash
        :blocks =>'',
        :slam_dunks =>''}
     ]
-  },
-  :away => {}
   }
+}
 end
